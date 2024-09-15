a1cc5db0e0decaadfed70c235282a330a1a41803
swapnilgupta14 <swapnilguptaswag@gmail.com>
2024-09-15 18:57:17 +0530

    additions: pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./styles/global.scss";
 import Homepage from "./pages/dashboard";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar />
       <Homepage />
     </>
   );
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,8 +1,16 @@
+import MatchCards from "../components/MatchCards";
+import { cricketMatches } from "../utils/DummyData/cricketMatches";
+import Navbar from "../components/Navbar";
+
 const Dashboard = () => {
   return (
-    <div className="bg-primary text-font-primary h-screen w-full">
-      <div className="bg-transparent h-1/2"></div>
-      <div className="bg-background h-2/3 rounded-t-3xl"> Hiii </div>
+    <div className="flex flex-col justify-center items-center text-font-primary h-screen w-full bg-dashboard-bg bg-cover bg-center">
+      <Navbar />
+      <div className="h-1/3 w-[96%] flex justify-center items-center p-1">
+        <MatchCards cricketMatches={cricketMatches} />
+      </div>
+
+      <div className="bg-background flex-1 w-[100%] rounded-t-3xl"> Hiii </div>
     </div>
   );
 };
